f2f329a9be47ba93c555d438dfd81154c8d57853
EdoFendy <edoardoatria14@gmail.com>
2025-10-15 02:32:11 +0200

    fixed lmobile layout

--- components/layout-wrapper.tsx
+++ components/layout-wrapper.tsx
@@ -7,17 +7,17 @@ interface LayoutWrapperProps {
 
 export function LayoutWrapper({ children, className = "" }: LayoutWrapperProps) {
   return (
-    <div className={`grid grid-cols-9 gap-4 ${className}`}>
+    <div className={`grid grid-cols-1 gap-4 md:grid-cols-9 ${className}`}>
       {/* Colonna vuota sinistra */}
-      <div className="col-span-1" />
-      
+      <div className="hidden md:col-span-1 md:block" />
+
       {/* Contenuto principale - 7 colonne centrali */}
-      <div className="col-span-7">
+      <div className="col-span-1 md:col-span-7">
         {children}
       </div>
-      
+
       {/* Colonna vuota destra */}
-      <div className="col-span-1" />
+      <div className="hidden md:col-span-1 md:block" />
     </div>
   )
 }
@@ -25,9 +25,9 @@ export function LayoutWrapper({ children, className = "" }: LayoutWrapperProps) 
 // Variante per sezioni che devono occupare tutta la larghezza
 export function FullWidthLayoutWrapper({ children, className = "" }: LayoutWrapperProps) {
   return (
-    <div className={`grid grid-cols-9 gap-4 ${className}`}>
+    <div className={`grid grid-cols-1 gap-4 md:grid-cols-9 ${className}`}>
       {/* Contenuto a tutta larghezza */}
-      <div className="col-span-9">
+      <div className="col-span-1 md:col-span-9">
         {children}
       </div>
     </div>
@@ -37,17 +37,17 @@ export function FullWidthLayoutWrapper({ children, className = "" }: LayoutWrapp
 // Variante per sezioni con contenuto centrato ma con elementi decorativi laterali
 export function CenteredLayoutWrapper({ children, className = "" }: LayoutWrapperProps) {
   return (
-    <div className={`grid grid-cols-9 gap-4 ${className}`}>
+    <div className={`grid grid-cols-1 gap-4 md:grid-cols-9 ${className}`}>
       {/* Colonna sinistra per elementi decorativi */}
-      <div className="col-span-1" />
-      
+      <div className="hidden md:col-span-1 md:block" />
+
       {/* Contenuto principale - 7 colonne centrali */}
-      <div className="col-span-7">
+      <div className="col-span-1 md:col-span-7">
         {children}
       </div>
-      
+
       {/* Colonna destra per elementi decorativi */}
-      <div className="col-span-1" />
+      <div className="hidden md:col-span-1 md:block" />
     </div>
   )
 }
